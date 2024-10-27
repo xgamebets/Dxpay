1367b49537cb9e95823cb0047d33e0cc480d3fcd
Luis Arthur Cruz Sousa <luisarthurcruzsousa@192.168.100.3>
2024-10-26 23:30:56 -0300

    feat: mobile and dashboard view

--- vendor/filament/widgets/resources/views/stats-overview-widget.blade.php
+++ vendor/filament/widgets/resources/views/stats-overview-widget.blade.php
@@ -2,16 +2,16 @@
     $columns = $this->getColumns();
 @endphp
 
-<x-filament-widgets::widget class="fi-wi-stats-overview">
-    <div
-        @if ($pollingInterval = $this->getPollingInterval())
-            wire:poll.{{ $pollingInterval }}
-        @endif
-        @class([
-            'fi-wi-stats-overview-stats-ctn grid gap-6'
-        ])
-        style="grid-template-columns: repeat(6, 1fr);"
-    >
+<x-filament-widgets::widget class=" fi-wi-stats-overview">
+    <div @if ($pollingInterval = $this->getPollingInterval()) wire:poll.{{ $pollingInterval }} @endif @class(['hidden sm:grid  fi-wi-stats-overview-stats-ctn  gap-6'])
+        style="grid-template-columns: repeat(5, 1fr);">
+        @foreach ($this->getCachedStats() as $stat)
+            {{ $stat }}
+        @endforeach
+    </div>
+
+    <div @if ($pollingInterval = $this->getPollingInterval()) wire:poll.{{ $pollingInterval }} @endif @class(['grid sm:hidden fi-wi-stats-overview-stats-ctn gap-6'])
+        style="grid-template-columns: repeat(1, 1fr);">
         @foreach ($this->getCachedStats() as $stat)
             {{ $stat }}
         @endforeach
